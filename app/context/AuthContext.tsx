2ce51b9206a9a734b01873e9287db6196e24b92a
abkaaar <abdullahabubakar461@gmail.com>
2025-04-21 01:46:04 +0100

    chore: remove Firebase configuration from AuthContext for cleaner code

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -13,15 +13,7 @@ import { useRouter, usePathname } from 'next/navigation';
 import { getFirestore, doc, setDoc, getDoc, serverTimestamp, Firestore } from 'firebase/firestore';
 import { auth, db } from '@/utils/firebase';
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
-};
+
 
 
 // Protected routes that require authentication
